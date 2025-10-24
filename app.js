5d272fd1c530e97e1cd247e251c8eee80b044bf6
faizn <bagusfaize@gmail.com>
2025-10-24 17:58:27 +0700

    feat: add home landing page

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 $(document).ready(function () {
+  const videoURL = "https://www.youtube.com/embed/-cjzfvV866E?autoplay=1";
+
   // Toggle menu for mobile view
   $(".menu-toggle").click(function () {
     $(".nav-links").toggleClass("active");
+  });
+
+  // Video modal handler
+  $("#watchVideoBtn").on("click", function () {
+    $("#videoModal").addClass("show")
+    $("#youtubeVideo").attr("src", videoURL);
+  });
+
+  $(".video-close, #videoModal").on("click", function (e) {
+    if (e.target !== this) return;
+    $("#videoModal").removeClass("show");
+    $("#youtubeVideo").attr("src", "");
   });
 
 
